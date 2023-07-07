a4e00d5abc071ca583bed8d9edfa00a66c70d3e9
SeungWon <pukuba@kakao.com>
2023-07-07 21:24:38 +0900

    feat: Add req id generation to fastify config

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import "@lib/dotenv"
 
 import fastify from "fastify"
+import hyperid from "hyperid"
 import { mercuriusRegister } from "./graphql"
 import { loggerOptions } from "./common/lib/logger"
 
 const app = fastify({
   logger: loggerOptions,
+  genReqId: () => hyperid().uuid,
 })
 
 app.get("/healthz", (req, reply) => {
