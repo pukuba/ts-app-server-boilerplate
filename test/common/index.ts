5277d28c359269ba4aa3d811d38cef3a4ef46feb
SeungWon <pukuba@kakao.com>
2023-07-08 03:32:01 +0900

    fix: lint

--- test/common/index.ts
+++ test/common/index.ts
@@ -2,13 +2,13 @@ import { schema } from "../../src/graphql/schema";
 import { GraphQLError, graphql } from "graphql";
 
 export const execute = async <T>(query: string, variables: Record<string, unknown> | null): Promise<[T, null] | [null, readonly GraphQLError[]]> => {
-  const { data, errors } =await graphql({
+  const { data, errors } = await graphql({
     schema,
     source: query,
     variableValues: variables,
   });
 
-  return errors ?  [null, errors] : [data as T, null];
+  return errors ? [null, errors] : [data as T, null];
 };
 
 export const gql = (strings: TemplateStringsArray): string => strings.join("");
