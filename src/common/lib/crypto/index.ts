fcb74136ace4fdf55369203e8461a86711a3bc20
SeungWon <pukuba@kakao.com>
2023-07-11 00:54:57 +0900

    feat: Add encryption-related utility functions

---
+++ src/common/lib/crypto/index.ts
@@ -0,0 +1,20 @@
+import { genSalt, hash, compare } from "bcryptjs";
+
+export const createHash = async(input: string): Promise<string> => {
+  const salt = await genSalt(10);
+  const hashed = await hash(input, salt);
+  return hashed;
+};
+
+export const compareHash = async(input: string, hashed: string): Promise<boolean> => {
+  const isMatch = await compare(input, hashed);
+  return isMatch;
+};
+
+export const decodeBase64 = (input: string): string => {
+  return Buffer.from(input, "base64").toString("utf-8");
+};
+
+export const encodeBase64 = (input: string): string => {
+  return Buffer.from(input).toString("base64");
+};
